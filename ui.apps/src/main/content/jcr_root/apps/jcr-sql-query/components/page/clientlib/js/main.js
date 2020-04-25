433fecb7822c7a68bdc85b53caca243994c80857
theopendle <theopendle@gmail.com>
2020-04-25 18:56:27 +0200

    Implement result table

--- ui.apps/src/main/content/jcr_root/apps/jcr-sql-query/components/page/clientlib/js/main.js
+++ ui.apps/src/main/content/jcr_root/apps/jcr-sql-query/components/page/clientlib/js/main.js
@@ -1,6 +1,6 @@
 (function () {
     const QUERY_FIELD_ID = "query-field";
-    const RESULT_TABLE_ID = "result-table";
+    const RESULT_CONTAINER_ID = "result-container";
     const FEEDBACK_ALERT_CONTENT_ID = "feedback-alert-content";
 
     window.app = {
@@ -19,23 +19,25 @@
                     const alertContent = document.getElementById(FEEDBACK_ALERT_CONTENT_ID);
                     if (!alertContent) {
                         console.error(`Cannot find feedback alert by id '${FEEDBACK_ALERT_CONTENT_ID}'`);
+                        return;
+                    }
+
+                    const tableContainer = document.getElementById(RESULT_CONTAINER_ID);
+                    if (!tableContainer) {
+                        console.error(`Cannot find result table by id '${RESULT_CONTAINER_ID}'`);
+                        return;
                     }
 
                     // If bad request, show feedback to user
-                    if (response.status === 400) {
+                    if (response.status !== 200) {
                         response.json().then(function (data) {
                             if (data.feedback) {
                                 alertContent.innerText = data.feedback;
                             }
 
                             alertContent.parentElement.removeAttribute("hidden");
+                            tableContainer.innerHTML = '';
                         });
-                        return;
-                    }
-
-                    // If unknown error code, log error
-                    if (response.status !== 200) {
-                        console.error('Error querying servlet. HTTP code: ' + response.status);
                         return;
                     }
 
@@ -43,13 +45,27 @@
                     response.json().then(function (data) {
                         alertContent.parentElement.setAttribute("hidden", "true");
 
-                        const table = document.getElementById(RESULT_TABLE_ID);
-                        if (!table) {
-                            console.error(`Cannot find result table by id '${RESULT_TABLE_ID}'`);
-                        }
+                        tableContainer.innerHTML = `
+                            <table id="result-table" 
+                                   class="table-example"
+                                   is="coral-table"
+                                   selectable>
+                                 <thead is="coral-table-head">
+                                        <tr is="coral-table-row">
+                                            ${data.headers.map(header => `
+                                            <th is="coral-table-headercell">${header}</th>
+                                            `).join('')}
+                                        </tr>
+                                </thead>
+                                <tbody is="coral-table-body">
+                                ${data.rows.map(row => `
+                                    <tr is="coral-table-row">
+                                        ${row.values.map(value => `<td is="coral-table-cell">${value}</td>`).join('')}
+                                    </tr>
+                                `).join('')}
+                                </tbody>
+                            </table>`;
                     });
-
-
                 })
                 .catch(error => {
                     console.log('Fetch error: ', error);
