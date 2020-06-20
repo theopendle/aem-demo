a24e419a08f79d519b2c959b353552c9deb19585
theopendle <theopendle@gmail.com>
2020-06-20 22:36:24 +0200

    Switch from CSS to JS clientlib

---
+++ ui.apps/src/main/content/jcr_root/apps/demo/clientlibs/clientlib-emulator/js/index.js
@@ -0,0 +1,5 @@
+(function () {
+    if (window.location.pathname.startsWith("/editor.html/content/demo")) {
+        document.querySelector(".editor-EmulatorDeviceRotate").setAttribute("hidden", "true");
+    }
+})();
