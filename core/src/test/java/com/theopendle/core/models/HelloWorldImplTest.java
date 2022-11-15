7cc5255302f6348d1118e5b8ccfc9e7499023fc8
Theo Pendle <theopendle@gmail.com>
2022-11-15 03:45:42 +0100

    Simplify HelloWorld model

--- core/src/test/java/com/theopendle/core/models/HelloWorldImplTest.java
+++ core/src/test/java/com/theopendle/core/models/HelloWorldImplTest.java
@@ -15,52 +15,43 @@
  */
 package com.theopendle.core.models;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.resource.Resource;
+import com.day.cq.wcm.api.Page;
+import com.theopendle.core.testcontext.AppAemContext;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.day.cq.wcm.api.Page;
-import io.wcm.testing.mock.aem.junit5.AemContext;
-import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import com.theopendle.core.testcontext.AppAemContext;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Simple JUnit test verifying the HelloWorldModel
  */
 @ExtendWith(AemContextExtension.class)
-class HelloWorldModelTest {
+class HelloWorldImplTest {
 
     private final AemContext context = AppAemContext.newAemContext();
 
-    private HelloWorldModel hello;
-
-    private Page page;
-    private Resource resource;
+    private HelloWorldImpl model;
 
     @BeforeEach
-    public void setup() throws Exception {
+    public void setup() {
 
         // prepare a page with a test resource
-        page = context.create().page("/content/mypage");
-        resource = context.create().resource(page, "hello",
-            "sling:resourceType", "demo/components/helloworld");
+        final Page page = context.create().page("/content/mypage");
+        context.create().resource(page, "hello",
+                "sling:resourceType", "demo/components/helloworld");
 
-        // create sling model
-        hello = resource.adaptTo(HelloWorldModel.class);
+        model = context.request().adaptTo(HelloWorldImpl.class);
     }
 
     @Test
-    void testGetMessage() throws Exception {
+    void testGetMessage() {
         // some very basic junit tests
-        String msg = hello.getMessage();
+        final String msg = model.getMessage();
         assertNotNull(msg);
-        assertTrue(StringUtils.contains(msg, resource.getResourceType()));
-        assertTrue(StringUtils.contains(msg, page.getPath()));
+        assertEquals("Hello world", msg);
     }
-
 }
